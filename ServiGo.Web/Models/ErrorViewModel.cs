fffb4d4d4ff22770e866aa6a863e0514e8b5a2a3
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-04-04 22:34:58 -0500

    Add ServiGo.Common and ServiGo.Web projects

---
+++ ServiGo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServiGo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
