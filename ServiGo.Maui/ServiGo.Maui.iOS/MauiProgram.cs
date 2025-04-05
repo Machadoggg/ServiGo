48e30303f23419ef9841fc376c0d2a9e4832f151
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-04-04 23:49:19 -0500

    Add MAUI project

---
+++ ServiGo.Maui/ServiGo.Maui.iOS/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiGo.Maui.iOS
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
